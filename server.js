1488ff89c609f366be3ef1a90f99aa4517f73ca8
Calder West <cw062@uark.edu>
2023-09-02 19:27:06 -0400

    changed app port to port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 
 const app = require('./app');
-const port = process.env.APP_PORT;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`); 
 });
