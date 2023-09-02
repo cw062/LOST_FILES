3b6ea124b1fdb2416c621b47d86c9d6a5edc3f3c
Calder West <cw062@uark.edu>
2023-09-02 19:53:36 -0400

    added procfile

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const app = require('./app');
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Now listening on port ${port}`); 
 });
 
