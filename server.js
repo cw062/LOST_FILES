58d38741110355c4adc577ad6b920959615b423b
Calder West <cw062@uark.edu>
2023-09-02 19:35:48 -0400

    changed portttt

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 
 const app = require('./app');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`); 
 });
